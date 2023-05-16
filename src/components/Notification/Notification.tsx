485a9936bea5b68db669b1cdfb0629d5c7ba61ee
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-05-15 23:48:22 -0300

    end

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -1,5 +1,5 @@
-import { OneNotify } from '@/store/notifications';
 import { useBearStore } from '@/store/store';
+import { OneNotify } from '@/store/notifications';
 import React from 'react';
 
 interface Props {
