2021e278b9663edf433bb9e67c45411fcf8fbc2f
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-05-16 03:33:59 -0300

    responsive

--- src/components/Movements/Movements.tsx
+++ src/components/Movements/Movements.tsx
@@ -3,6 +3,7 @@ import { terminal } from '@/utils/image-cloudinary';
 import TitleTyping from '../TitleTyping/TitleTyping';
 import { Historial } from '@/games/types/types';
 import CardHistorial from '../CardHistorial/CardHistorial';
+import { useBearStore } from '@/store/store';
 
 type Props = {
 	historial: (string | Historial)[];
@@ -11,18 +12,18 @@ type Props = {
 const title = 'Movimientos realizados';
 export default function Movements({ historial }: Props) {
 	const scrollRef = useRef<HTMLDivElement | null>(null);
+	const { openTerminal } = useBearStore((state) => state);
 	useEffect(() => {
 		if (scrollRef.current !== null) {
 			scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
 		}
 	}, [historial]);
 	return (
-		<div className="card__movements">
+		<div className="card__movements" data-x={openTerminal}>
 			<TitleTyping text={title} image={terminal} />
 			<section className="historial" ref={scrollRef}>
 				{historial.length === 0 && (
 					<div className="historial__cero">
-						{' '}
 						<TitleTyping text="Sin movimientos." />
 					</div>
 				)}
